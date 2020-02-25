7ed6d6db2aa5f2213805f7f0a9ad208637f7c88b
berkut126 <berkut126@gmail.com>
2020-02-25 07:32:28 +0300

    Finalized the program. Found problems with boost. In the future, dockerfile shall be edited for improving boost process.

--- include/header.hpp
+++ include/header.hpp
@@ -1,6 +1,24 @@
 // Copyright 2018 Your Name <your_email>
+#include <string>
+#include <chrono>
+#include <iostream>
+#include <thread>
+#include <future>
 
-#ifndef INCLUDE_HEADER_HPP_
-#define INCLUDE_HEADER_HPP_
+#include <async++.h>
 
-#endif // INCLUDE_HEADER_HPP_
+#include <boost/program_options.hpp>
+#include <boost/filesystem.hpp>
+
+#include <boost/process/child.hpp>
+#include <boost/process.hpp>
+#include <boost/process/pipe.hpp>
+#include <boost/process/io.hpp>
+#include <boost/process/group.hpp>
+
+using boost::program_options::options_description;
+using boost::program_options::variables_map;
+using boost::program_options::error;
+using boost::program_options::store;
+using boost::program_options::notify;
+using boost::program_options::value;
